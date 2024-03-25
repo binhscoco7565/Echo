b9654d3d6b4ca65d580a0b14baede18bdf069b7a
binhscoco7565 <lastpass7565@gmail.com>
2024-03-25 19:42:33 +1000

    Changed requirements, added unimplemented message and added app packaging on actions

--- scripts/elevated/privacy.py
+++ scripts/elevated/privacy.py
@@ -4,8 +4,7 @@ import dearpygui.dearpygui as dpg
 def privacy():
     with dpg.tree_node(label='Install and run privacy.sexy'):
         with dpg.group(tag='privacy_group'):
-            dpg.add_text('test')
-            dpg.add_button(label='test')
+            dpg.add_text('Currently unimplemented...c')
 
         # Here it is again...
         dpg.add_text('\n')
